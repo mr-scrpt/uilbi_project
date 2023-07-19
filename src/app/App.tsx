26a0d84d252537aaf9093e34f45de8aa4ce8a86d
mr.scrpt <mr.scrpt@gmail.com>
2023-07-19 11:23:08 +0300

    fix: stylelint standart config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,31 @@
-import './style/index.scss';
-import { useTheme } from 'app/provider/ThemeProvider';
-import { classNames } from 'shared/lib/classNames';
-import { Navbar } from 'widget/Navbar';
-import { Sidebar } from 'widget/Sidebar';
-import { Suspense, useState } from 'react';
-import cls from './App.module.scss';
-import { AppRouter } from './provider/router';
+import './style/index.scss'
+import { useTheme } from 'app/provider/ThemeProvider'
+import { classNames } from 'shared/lib/classNames'
+import { Navbar } from 'widget/Navbar'
+import { Sidebar } from 'widget/Sidebar'
+import { Suspense, useState } from 'react'
+import cls from './App.module.scss'
+import { AppRouter } from './provider/router'
 
 function App() {
-  const { theme } = useTheme();
+  const { theme } = useTheme()
 
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false)
 
   const toggleHandler = () => {
-    setCollapsed((prev) => !prev);
-  };
+    setCollapsed((prev) => !prev)
+  }
 
-  const classApp = classNames(cls.app, ['theme', theme], {});
+  const classApp = classNames(cls.app, ['theme', theme], {})
 
   const clsSidebar = classNames(cls.boxSidebar, [], {
     [cls.boxSidebar_collapse]: collapsed,
     [cls.boxSidebar_open]: !collapsed,
-
-  });
+  })
 
   return (
     <div className={classApp}>
       <Suspense fallback="transslation">
-
         <div className={cls.inner}>
           <Navbar className={cls.boxNavbar} />
 
@@ -39,9 +37,8 @@ function App() {
           <div className={cls.boxFooter}>footer</div>
         </div>
       </Suspense>
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,17 @@
-const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate')
-
+// const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate')
+//
+// module.exports = {
+//   extends: ['stylelint-config-standard-scss'],
+//   plugins: ['stylelint-order'],
+//   rules: {
+//     'order/properties-order': [
+//       sortOrderSmacss({ emptyLineBefore: 'always', noEmptyLineBetween: true }),
+//     ],
+//   },
+// }
 module.exports = {
-  extends: ['stylelint-config-property-sort-order-smacss'],
-  plugins: ['stylelint-order'],
-  rules: {
-    'order/properties-order': [
-      sortOrderSmacss({ emptyLineBefore: 'always', noEmptyLineBetween: true }),
-    ],
-  },
+  extends: [
+    'stylelint-config-clean-order',
+    'stylelint-config-recommended-scss',
+  ],
 }
