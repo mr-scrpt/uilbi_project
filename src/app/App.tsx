caf0e08bf6abb7ca0e59e9786fb866bf342538bd
mr.scrpt <mr.scrpt@gmail.com>
2023-07-19 14:43:45 +0300

    implement: jest m2-l20

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,13 @@
 import './style/index.scss'
 import { useTheme } from 'app/provider/ThemeProvider'
+import { useTranslation } from 'react-i18next'
+
 import { classNames } from 'shared/lib/classNames'
 import { Navbar } from 'widget/Navbar'
 import { Sidebar } from 'widget/Sidebar'
 import { Suspense, useState } from 'react'
 import cls from './App.module.scss'
+
 import { AppRouter } from './provider/router'
 
 function App() {
@@ -15,6 +18,8 @@ function App() {
   const toggleHandler = () => {
     setCollapsed((prev) => !prev)
   }
+
+  const { t } = useTranslation()
 
   const classApp = classNames(cls.app, ['theme', theme], {})
 
@@ -28,13 +33,12 @@ function App() {
       <Suspense fallback="transslation">
         <div className={cls.inner}>
           <Navbar className={cls.boxNavbar} />
-
+          {t('dddd')}
           <div className={cls.boxMain}>
             <Sidebar className={clsSidebar} toggleHandler={toggleHandler} />
             <AppRouter className={cls.boxContent} />
           </div>
-
-          <div className={cls.boxFooter}>footer</div>
+          <div className={cls.boxFooter}>{t('footer')}</div>
         </div>
       </Suspense>
     </div>
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,14 +1,14 @@
-import { ClassModType } from './type';
+import { ClassModType } from './type'
 
 export const classNames = (
   classMain: string,
   classOther: string[] = [],
-  classMod: ClassModType = {},
-): string => [
-  classMain,
-  ...classOther.filter(Boolean),
-  ...Object
-    .entries(classMod)
-    .filter(([_, classValue]) => Boolean(classValue))
-    .map(([className]) => className),
-].join(' ');
+  classMod: ClassModType = {}
+): string =>
+  [
+    classMain,
+    ...classOther.filter(Boolean),
+    ...Object.entries(classMod)
+      .filter(([, classValue]) => Boolean(classValue))
+      .map(([className]) => className),
+  ].join(' ')
