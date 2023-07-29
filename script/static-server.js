5c723fb323aaae0cd8eb2a35f1a89332bbdc2e95
mr.scrpt <mr.scrpt@gmail.com>
2023-07-29 12:26:05 +0300

    add: visual check ui screeen shots

---
+++ script/static-server.js
@@ -0,0 +1,39 @@
+const StaticServer = require('static-server')
+
+const server = new StaticServer({
+  rootPath: '.loki/', // required, the root of the server file tree
+  port: 1337, // required, the port to listen
+  name: 'my-http-server', // optional, will set "X-Powered-by" HTTP header
+  host: 'localhost', // optional, defaults to any interface
+  cors: '*', // optional, defaults to undefined
+  followSymlink: true, // optional, defaults to a 404 error
+  templates: {
+    index: 'report.html', // optional, defaults to 'index.html'
+    notFound: '404.html', // optional, defaults to undefined
+  },
+})
+
+server.start(() => {
+  console.log('Server listening to', server.port)
+})
+
+server.on('request', (req, res) => {
+  // console.log('', req)
+  // req.path is the URL resource (file name) from server.rootPath
+  // req.elapsedTime returns a string of the request's elapsed time
+})
+
+server.on('symbolicLink', (link, file) => {
+  // link is the source of the reference
+  // file is the link reference
+  console.log('File', link, 'is a link to', file)
+})
+
+server.on('response', (req, res, err, file, stat) => {
+  // res.status is the response status sent to the client
+  // res.headers are the headers sent
+  // err is any error message thrown
+  // file the file being served (may be null)
+  // stat the stat of the file being served (is null if file is null)
+  // NOTE: the response has already been sent at this point
+})
