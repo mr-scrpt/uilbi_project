945b36a58843ac71695f5d15ed4eaf46bdd25d0a
mr.scrpt <mr.scrpt@gmail.com>
2023-08-18 15:07:14 +0300

    fix: login thunk test

--- src/feature/AuthByUserName/model/service/login/login.test.ts
+++ src/feature/AuthByUserName/model/service/login/login.test.ts
@@ -12,13 +12,13 @@ const mockAxios = jest.mocked(axios)
 describe('getLoginError', () => {
   test('should post method been called', async () => {
     const userData = { username: 'test', id: '123' }
-    mockAxios.post.mockResolvedValue(
+    const thunk = new AsyncThunk(loginByUserName)
+    thunk.api.post.mockResolvedValue(
       Promise.resolve({
         data: userData,
       })
     )
 
-    const thunk = new AsyncThunk(loginByUserName)
     await thunk.callThunk({
       username: 'test',
       password: 'test',
@@ -27,75 +27,76 @@ describe('getLoginError', () => {
     expect(mockAxios.post).toHaveBeenCalled()
   })
 
-  // test('should have fulfilled status', async () => {
-  //   const userData = { username: 'test', id: '123' }
-  //   mockAxios.post.mockResolvedValue(
-  //     Promise.resolve({
-  //       data: userData,
-  //     })
-  //   )
-  //
-  //   const thunk = new AsyncThunk(loginByUserName)
-  //   const result = await thunk.callThunk({
-  //     username: 'test',
-  //     password: 'test',
-  //   })
-  //
-  //   expect(thunk.dispatch).toHaveBeenCalledTimes(4)
-  //   expect(result.meta.requestStatus).toBe('fulfilled')
-  //   expect(result.payload).toBe(userData)
-  // })
-  //
-  // test('should been return rejected status', async () => {
-  //   mockAxios.post.mockResolvedValue(
-  //     Promise.resolve({
-  //       status: 403,
-  //     })
-  //   )
-  //   const thunk = new AsyncThunk(loginByUserName)
-  //   const result = await thunk.callThunk({
-  //     username: 'test',
-  //     password: 'test',
-  //   })
-  //
-  //   expect(thunk.dispatch).toHaveBeenCalledTimes(2)
-  //   expect(result.meta.requestStatus).toBe('rejected')
-  //   expect(result.payload).toBe('api-error-login-message')
-  // })
-  //
-  // test('should been called dispatch inside thunk => set auth data', async () => {
-  //   const userData = { username: 'test', id: '123' }
-  //   mockAxios.post.mockResolvedValue(
-  //     Promise.resolve({
-  //       data: userData,
-  //     })
-  //   )
-  //
-  //   const thunk = new AsyncThunk(loginByUserName)
-  //   await thunk.callThunk({
-  //     username: 'test',
-  //     password: 'test',
-  //   })
-  //
-  //   expect(thunk.dispatch).toHaveBeenCalledWith(userAction.setAuth(userData))
-  // })
-  //
-  // test('should been called dispatch inside thunk => set modal data', async () => {
-  //   const userData = { username: 'test', id: '123' }
-  //   mockAxios.post.mockResolvedValue(
-  //     Promise.resolve({
-  //       data: userData,
-  //     })
-  //   )
-  //
-  //   const thunk = new AsyncThunk(loginByUserName)
-  //   await thunk.callThunk({
-  //     username: 'test',
-  //     password: 'test',
-  //   })
-  //
-  //   expect(thunk.dispatch).toHaveBeenCalledWith(
-  //     modalAction.closeModal({ name: ModalNameEnum.LOGIN })
-  //   )
-  // })
+  test('should have fulfilled status', async () => {
+    const userData = { username: 'test', id: '123' }
+    const thunk = new AsyncThunk(loginByUserName)
+    thunk.api.post.mockResolvedValue(
+      Promise.resolve({
+        data: userData,
+      })
+    )
+    const result = await thunk.callThunk({
+      username: 'test',
+      password: 'test',
+    })
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(4)
+    expect(result.meta.requestStatus).toBe('fulfilled')
+    expect(result.payload).toBe(userData)
+  })
+
+  test('should been return rejected status', async () => {
+    const thunk = new AsyncThunk(loginByUserName)
+    thunk.api.post.mockResolvedValue(
+      Promise.resolve({
+        status: 403,
+      })
+    )
+
+    const result = await thunk.callThunk({
+      username: 'test',
+      password: 'test',
+    })
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2)
+    expect(result.meta.requestStatus).toBe('rejected')
+    expect(result.payload).toBe('api-error-login-message')
+  })
+
+  test('should been called dispatch inside thunk => set auth data', async () => {
+    const userData = { username: 'test', id: '123' }
+    const thunk = new AsyncThunk(loginByUserName)
+    thunk.api.post.mockResolvedValue(
+      Promise.resolve({
+        data: userData,
+      })
+    )
+
+    await thunk.callThunk({
+      username: 'test',
+      password: 'test',
+    })
+
+    expect(thunk.dispatch).toHaveBeenCalledWith(userAction.setAuth(userData))
+  })
+
+  test('should been called dispatch inside thunk => set modal data', async () => {
+    const userData = { username: 'test', id: '123' }
+    const thunk = new AsyncThunk(loginByUserName)
+    thunk.api.post.mockResolvedValue(
+      Promise.resolve({
+        data: userData,
+      })
+    )
+
+    await thunk.callThunk({
+      username: 'test',
+      password: 'test',
+    })
+
+    expect(thunk.dispatch).toHaveBeenCalledWith(
+      modalAction.closeModal({ name: ModalNameEnum.LOGIN })
+    )
+  })
+>>>>>>> 6266c13 (fix: login thunk test)
 })
