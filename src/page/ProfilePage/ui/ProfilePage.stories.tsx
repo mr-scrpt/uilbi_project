fbe87f4df8174a998e9a2cbde436def9ff174ad3
mr.scrpt <mr.scrpt@gmail.com>
2023-10-23 13:24:06 +0300

    Удалена папка .yarn из отслеживания Git

--- src/page/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/page/ProfilePage/ui/ProfilePage.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { StateSchema } from 'app/provider/StoreProvider'
+import { CountryEnum } from 'shared/const/country.enum'
+import { CurrencyEnum } from 'shared/const/currency.enum'
+import { RoleEnum } from 'shared/type/role/role.enum'
 
 import {
   RouterDecorator,
@@ -21,5 +24,71 @@ type Story = StoryObj<typeof meta>
 
 export const Base: Story = {
   args: {},
-  decorators: [RouterDecorator, StoreDecorator({} as StateSchema)],
+  parameters: {
+    mockData: [
+      {
+        url: `${__API_URL__}/country`,
+        method: 'GET',
+        status: 200,
+        response: [
+          {
+            name: 'Ukraine',
+            abb: 'UA',
+          },
+          {
+            name: 'Poland',
+            abb: 'PL',
+          },
+          {
+            name: 'United Kindom',
+            abb: 'UK',
+          },
+          {
+            name: 'United State America',
+            abb: 'USA',
+          },
+        ],
+      },
+      {
+        url: `${__API_URL__}/currency`,
+        method: 'GET',
+        status: 200,
+        response: [
+          {
+            name: 'Hrivna',
+            abb: 'UAH',
+          },
+          {
+            name: 'Zlotiy',
+            abb: 'PLN',
+          },
+          {
+            name: 'Pound',
+            abb: 'GBP',
+          },
+          {
+            name: 'Dollar',
+            abb: 'USD',
+          },
+        ],
+      },
+    ],
+  },
+  decorators: [
+    RouterDecorator,
+    StoreDecorator({
+      profile: {
+        data: {
+          id: '1',
+          firstname: 'user',
+          lastname: 'lastname',
+          age: 30,
+          currency: CurrencyEnum.EUR,
+          country: CountryEnum.UA,
+          city: 'Kharkiv',
+          role: RoleEnum.ADMIN,
+        },
+      },
+    } as StateSchema),
+  ],
 }
